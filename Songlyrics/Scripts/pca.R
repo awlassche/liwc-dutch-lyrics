7ca442f3f3fe796a7370ff27d36230b7d6328775
Alie Lassche <a.w.lassche@uu.nl>
2019-01-25 14:39:59 +0100

    plots, updated analysis

--- Songlyrics/Scripts/pca.R
+++
@@ -1,4 +0,0 @@
-install.packages('ggfortify')
-library('ggfortify')
-df2 <- read.csv('/Users/alielassche/documents/github/cultural-analytics/songlyrics/negpos.csv', header = TRUE, row.names = 'Artist', sep = ';')
-autoplot(prcomp(df2), loadings = TRUE, label = TRUE, label.size = 3, shape = FALSE, loadings.label = TRUE, loadings.label.size = 3, loadings.colour = 'blue')
